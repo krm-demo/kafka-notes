a4efb4961f0ee1ca86fc2cfeb5b98bf695f6e90a
krm-demo <149853618+krm-demo@users.noreply.github.com>
2024-04-26 20:55:43 -0400

    Create JdbcHelper.java

---
+++ src/main/java/org/krmdemo/kafka/util/JdbcHelper.java
@@ -0,0 +1,29 @@
+package com.chase.digital.payment.wire.kafka.util.photonhelloworld.jdbc;
+
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.IOUtils;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.Charset;
+
+@Slf4j
+@Component
+public class JdbcHelper {
+
+    @Resource(name = "jdbc-template-embedded-H2")
+    JdbcTemplate jdbcTemplate;
+
+    public String createTableKafkaRecord() {
+        try {
+            // TODO: try " RUNSCRIPT FROM 'classpath:/.....' "
+            String sql = IOUtils.resourceToString("/jdbc/create-table--kafka-record.sql", Charset.defaultCharset());
+            jdbcTemplate.execute(sql);
+            return "";
+        } catch (Exception ex) {
+            log.error("could not create a table for kafka-record", ex);
+            return ex.getMessage();
+        }
+    }
+}
