7dec61c7915170e643d61cd3f31287d7af63117c
krm-demo <149853618+krm-demo@users.noreply.github.com>
2024-04-26 20:57:41 -0400

    Create EmbeddedDatabaseConfig.java

---
+++ src/main/java/org/krmdemo/kafka/util/inspect/app/EmbeddedDatabaseConfig.java
@@ -0,0 +1,37 @@
+package com.chase.digital.payment.wire.kafka.util.photonhelloworld;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+
+
+@Slf4j
+@Configuration
+public class EmbeddedDatabaseConfig {
+
+    final String DRIVER_H2 = "org.h2.Driver";
+
+    @Bean("data-source-embedded-H2")
+    public DataSource getDataSource() {
+        DataSourceBuilder<?> dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName(DRIVER_H2);
+        dataSourceBuilder.url("jdbc:h2:mem:hello-world");
+        dataSourceBuilder.username("SA");
+        dataSourceBuilder.password("");
+        DataSource dataSource = dataSourceBuilder.build();
+        log.info("DataSource for embedded H2 was created of type " + dataSource.getClass());
+        return dataSource;
+    }
+
+    @Bean("jdbc-template-embedded-H2")
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        // TODO: think about using connection-pool
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        log.info("JdbcTemplate is ready to use");
+        return jdbcTemplate;
+    }
+}
